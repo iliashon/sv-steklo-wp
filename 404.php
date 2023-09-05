b71c75b1c3dff66c7ca3ef7f7b31cd03b809784b
Tupalski Ilya <itupalski@gmail.com>
2023-09-05 15:47:20 +0400

    feat: add page 404 (Tue Sep  5 15:47:20 +04 2023)

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php 
+get_header();
+?>
+<main>
+  <section class="container">
+    <div class="error">
+      <h1>404</h1>
+      <h3>Ошибка!</h3>
+      <p>Такой страницы не существует</p>
+    </div>
+  </section>
+</main>
+  
+<?php get_footer(); ?>
