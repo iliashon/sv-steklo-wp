1ec999b568b2e4fa765f216ae31f21a5adf4ca70
Tupalski Ilya <itupalski@gmail.com>
2023-09-05 10:44:54 +0400

    init: creat repo and init project in wordpress (Tue Sep  5 10:44:54 +04 2023)

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../style.css" />
+  <title>Sv-steklo</title>
+</head>
+
+<body>
+  <header id="header">
+    <section class="container">
+      <div class="header-menu">
+        <a href="./home.html"><img id="logoheader" src="../assets/img/logoheader.png" alt="" /></a>
+        <nav id="nav-bar">
+          <a href="./home.html" class="nav-bar-a__active">ГЛАВНАЯ</a>
+          <a href="./products.html">ПРОДУКЦИЯ</a>
+          <a href="./catalog.html">КАТАЛОГ СТЕКЛА</a>
+          <a href="./stock.html">АКЦИИ</a>
+          <a href="./services.html">УСЛУГИ</a>
+          <a href="./about.html">О НАС</a>
+          <a href="./contacts.html">КОНТАКТЫ</a>
+        </nav>
+        <div id="button-burger">
+          <span class="button-burger-line"></span>
+        </div>
+        <nav id="nav-bar-burger">
+          <a href="./home.html" class="nav-bar-a__active">ГЛАВНАЯ</a>
+          <a href="./products.html">ПРОДУКЦИЯ</a>
+          <a href="./catalog.html">КАТАЛОГ СТЕКЛА</a>
+          <a href="./stock.html">АКЦИИ</a>
+          <a href="./services.html">УСЛУГИ</a>
+          <a href="./about.html">О НАС</a>
+          <a href="./contacts.html">КОНТАКТЫ</a>
+        </nav>
+      </div>
+    </section>
+  </header>
---
+++ index.php
@@ -0,0 +1,67 @@
+<main>
+  <section class="home-slider">
+    <div class="container">
+
+    </div>
+  </section>
+
+  <section class="products">
+    <div class="container">
+      <aside class="products-block-list">
+        <div class="name-product-list">
+          <h5>НАША ПРОДУКЦИЯ</h5>
+        </div>
+        <ul class="products-list">
+          <li class="products-item">
+            <a href="">
+              <img src="../assets/img/peskostrujnaja-obrabotka-diskov-1.jpg" alt="">
+              <h6>Скинали для кухни</h6>
+            </a>
+          </li>
+          <li class="products-item">
+            <a href="">
+              <img src="../assets/img/fonbg.jpg" alt="">
+              <h6>Стеклянные двери</h6>
+            </a>
+          </li>
+          <li class="products-item">
+            <a href="">
+              <img src="../assets/img/dfg.png" alt="">
+              <h6>Обеденные столы</h6>
+            </a>
+          </li>
+          <li class="products-item">
+            <a href="">
+              <img src="../assets/img/usl1.png" alt="">
+              <h6>Зеркала под заказ</h6>
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  </section>
+
+  <section class="home-advantages">
+    <div class="container">
+      <h2>НАША ПРОДУКЦИЯ</h2>
+      <div class="home-advantages-list">
+        <div class="advantages-item">
+          <img src="../assets/svg/factory-1067.svg" alt="">
+          <h5>СОВРЕМЕННОЕ СОБСТВЕННОЕ ПРОИЗВОДСТВО</h5>
+        </div>
+        <div class="advantages-item">
+          <img src="../assets/svg/book-4981.svg" alt="">
+          <h5>ЛЮБЫЕ ИЗДЕЛИЯ ПОД ЗАКАЗ</h5>
+        </div>
+        <div class="advantages-item">
+          <img src="../assets/svg/check-7047.svg" alt="">
+          <h5>ПОЛНЫЙ ЦИКЛ ОБРАБОТКИ СТЕКЛА</h5>
+        </div>
+        <div class="advantages-item">
+          <img src="../assets/svg/thumbs-up-11229.svg" alt="">
+          <h5>ГИБКИЕ УСЛОВИЯ ОПЛАТЫ</h5>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
