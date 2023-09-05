0b777a45acf805171a483e844b37e71d86d7958f
Tupalski Ilya <itupalski@gmail.com>
2023-09-05 15:05:46 +0400

    refactor: conected footer on all pages (Tue Sep  5 15:05:46 +04 2023)

--- templates/stock.php
+++ templates/stock.php
@@ -36,27 +36,4 @@ get_header();
       </section>
     </main>
 
-    <footer id="footer">
-      <section class="container">
-        <div class="footer-menu">
-          <div class="footer-info">
-            <h4>НАШ АДРЕС:</h4>
-            <p>Республика Беларусь,<br> г. Могилёв, ул. Чигринова, 7Б</p>
-          </div>
-          <div class="footer-info">
-            <h4>ЗВОНИТЕ НАМ:</h4>
-            <a href="tel:+375291943889">+375 (29) 194-38-89 Velcom</a><br>
-            <a href="tel:80222650642">8 (0222) 65-06-42 Тел/факс</a>
-          </div>
-          <div class="footer-info">
-            <h4>РЕЖИМ РАБОТЫ:</h4>
-            <p>Пн-Пт с 9.00 до 19.00<br>
-              Сб. с 10.00 до 16.00<br>
-              Вс. выходной</p>
-          </div>
-        </div>
-      </section>
-    </footer>
-    <script src="tmp/sv-cteklo/js/burger.js"></script>
-  </body>
-</html>
+    <?php get_footer(); ?>
